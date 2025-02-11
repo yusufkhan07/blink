15a3f8f9eac15ea50276404e905986043920cc4b
Yousuf Khan <yusufkhanjee@gmail.com>
2025-02-11 11:10:49 +0500

    add slack tokens to env file

--- apps/slack-app/src/main.ts
+++ apps/slack-app/src/main.ts
@@ -1,1 +1,16 @@
-console.log('Hello World');
+const { App } = require('@slack/bolt');
+
+console.log(process.env);
+
+// Initializes your app with your bot token and signing secret
+const app = new App({
+  token: process.env.SLACK_BOT_TOKEN,
+  signingSecret: process.env.SLACK_SIGNING_SECRET
+});
+
+(async () => {
+  // Start your app
+  await app.start(process.env.PORT || 3000);
+
+  app.logger.info('⚡️ Bolt app is running!');
+})();
