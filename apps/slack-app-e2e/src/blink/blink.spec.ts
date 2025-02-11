6b34d24ef1c08ed9d91a9f029151ede254b42825
Yousuf Khan <yusufkhanjee@gmail.com>
2025-02-11 13:00:04 +0500

    Disable e2e

--- apps/slack-app-e2e/src/blink/blink.spec.ts
+++ apps/slack-app-e2e/src/blink/blink.spec.ts
@@ -1,12 +1,13 @@
-import { execSync } from 'child_process';
+// import { execSync } from 'child_process';
 import { join } from 'path';
 
-describe('CLI tests', () => {
-  it('should print a message', () => {
-    const cliPath = join(process.cwd(), 'dist/apps/slack-app');
+// TODO: Implement
+// describe('CLI tests', () => {
+//   it('should print a message', () => {
+//     const cliPath = join(process.cwd(), 'dist/apps/slack-app');
 
-    const output = execSync(`node ${cliPath}`).toString();
+//     const output = execSync(`node ${cliPath}`).toString();
 
-    expect(output).toMatch(/Hello World/);
-  });
-});
+//     expect(output).toMatch(/Hello World/);
+//   });
+// });
