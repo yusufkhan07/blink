a6a0dac7dfdba68f389e42886e9ef2ac1a35767e
Yousuf Khan <yusufkhanjee@gmail.com>
2025-02-15 15:44:34 +0500

    Setup serverless local

--- apps/slack-app/src/main.ts
+++ apps/slack-app/src/main.ts
@@ -1,4 +1,4 @@
-import { App } from '@slack/bolt';
+import { App, AwsLambdaReceiver } from '@slack/bolt';
 import { configureCommands } from './commands';
 
 // TODO: setup prettier
@@ -12,17 +12,30 @@ import { configureCommands } from './commands';
 // User types /blink message expiry 1 hour
 // It expires after 1 hour.
 
+const awsLambdaReceiver = new AwsLambdaReceiver({
+  signingSecret: process.env.SLACK_SIGNING_SECRET,
+});
+
 // Initializes your app with your bot token and signing secret
 const app = new App({
   token: process.env.SLACK_BOT_TOKEN,
-  signingSecret: process.env.SLACK_SIGNING_SECRET
+  receiver: awsLambdaReceiver
 });
 
 configureCommands(app);
 
-(async () => {
-  // Start your app
-  await app.start(process.env.PORT || 3000);
+// (async () => {
+//   // Start your app
+//   await app.start(process.env.PORT || 3000);
 
-  app.logger.info('⚡️ Bolt app is running!');
-})();
+//   app.logger.info('⚡️ Bolt app is running!');
+// })();
+
+// TODO: Can we run serverless in debug mode?
+// How to run serverless with NX?
+// Serverless duplicate events
+
+module.exports.handler = async (event, context, callback) => {
+  const handler = await awsLambdaReceiver.start();
+  return handler(event, context, callback);
+}
