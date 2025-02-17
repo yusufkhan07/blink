b29017e7fc86cfa1b5abefe263e8e93c33fbf936
Yousuf Khan <yusufkhanjee@gmail.com>
2025-02-17 15:32:45 +0500

    add oauth flow with file store

---
+++ apps/slack-app/src/lambdas/slack-install-handler.ts
@@ -0,0 +1,21 @@
+import { ExpressReceiver, FileInstallationStore } from '@slack/bolt';
+import serverless from 'serverless-http';
+
+const expressReceiver = new ExpressReceiver({
+  signingSecret: process.env.SLACK_SIGNING_SECRET,
+  clientId: process.env.SLACK_CLIENT_ID,
+  clientSecret: process.env.SLACK_CLIENT_SECRET,
+  stateSecret: 'SLACK_STATE_SECRET',
+  // TODO: load from manifest file
+  scopes: ['chat:write', 'chat:write.public', 'commands'],
+  installerOptions: {
+    installPath: '/slack/install',
+    redirectUriPath: '/slack/oauth_redirect',
+    // TODO: this must be set to true
+    stateVerification: false,
+  },
+  // TODO: Store in DB instead of File
+  installationStore: new FileInstallationStore(),
+});
+
+module.exports.handler = serverless(expressReceiver.app);
