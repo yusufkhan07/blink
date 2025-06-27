4b4dc266b8c6b7f83b0860364209dba34d211e85
Yousuf Khan <yusufkhanjee@gmail.com>
2025-06-27 10:05:35 -0400

    update email

--- apps/blink-website/src/pages/ContactUs.tsx
+++ apps/blink-website/src/pages/ContactUs.tsx
@@ -2,7 +2,7 @@ export function ContactUs() {
   return (
     <section className="container mx-auto py-16 px-4 max-w-2xl">
       <h2 className="text-2xl font-bold mb-4 text-primary">Contact Us</h2>
-      <p>Contact us at <a href="mailto:support@example.com" className="text-primary underline">support@example.com</a></p>
+      <p>Contact us at <a href="mailto:hello@bytedevs.com" className="text-primary underline">hello@bytedevs.com</a></p>
     </section>
   );
 }
