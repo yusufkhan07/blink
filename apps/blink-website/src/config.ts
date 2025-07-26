ebeedea948cb11e0362355698790a7e92086251a
Yousuf Khan <yusufkhanjee@gmail.com>
2025-07-26 16:47:50 -0400

    fix url

--- apps/blink-website/src/config.ts
+++ apps/blink-website/src/config.ts
@@ -1,1 +1,12 @@
-export const SLACK_OAUTH_URL = "https://slack.com/oauth/v2/authorize?scope=chat%3Awrite%2Cchat%3Awrite.public%2Ccommands&client_id=819140066320.9092531390403";
+const SLACK_CLIENT_ID = '819140066320.9092531390403';
+const SLACK_SCOPES = [
+  'chat:write',
+  'chat:write.public',
+  'commands',
+  'chat:write.customize',
+  'users:read',
+];
+
+export const SLACK_OAUTH_URL = `https://slack.com/oauth/v2/authorize?scope=${encodeURIComponent(
+  SLACK_SCOPES.join(',')
+)}&client_id=${SLACK_CLIENT_ID}`;
