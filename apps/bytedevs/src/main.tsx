96ad6eaa30ab0039d9d51757f7ba470f03e275b9
Yousuf Khan <yusufkhanjee@gmail.com>
2025-12-01 18:35:41 -0500

    Bytedevs website redesign (#11)
    * Revert "Revert "Bytedevs website redesign (#8)" (#9)"
    
    This reverts commit d228f55e74ff881f17f684b8b53d462570227d1f.
    
    * Fix

--- apps/bytedevs/src/main.tsx
+++ apps/bytedevs/src/main.tsx
@@ -2,6 +2,7 @@ import { StrictMode } from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import * as ReactDOM from 'react-dom/client';
 import App from './app/app';
+import './styles.css';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
